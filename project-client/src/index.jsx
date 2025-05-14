e65edadb11897219fc1d4b2cb21ce06208073d6f
Cameron MacDonald <cameron.n.macdonald.26@dartmouth.edu>
2025-05-14 18:11:56 -0400

    connecting server client hello world

--- project-client/src/index.jsx
+++ project-client/src/index.jsx
@@ -13,3 +13,9 @@
  * - Performance monitoring
  * - Error reporting
  */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const root = createRoot(document.getElementById('main'));
+root.render(<App />);
