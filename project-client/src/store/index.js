a7bf4ef404780e527a44d3d273a196fad2857218
Neal <nealchopra06@gmail.com>
2025-05-21 13:29:04 -0400

    Add Zustand store setup with lesson and user slices for state management

---
+++ project-client/src/store/index.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import createLessonSlice from './lessonSlice';
+import createUserSlice from './userSlice';
+
+const useStore = create(
+  devtools(
+    (set, get) => ({
+      ...createLessonSlice(set, get),
+      ...createUserSlice(set, get),
+    }),
+    {
+      name: 'LessonLink Store',
+    },
+  ),
+);
+
+export default useStore; 
